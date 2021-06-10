74f8f2f84b9f02e34ed714a3f514de33f25dbe5b
Lars Kanis <kanis@comcard.de>
2021-06-10 15:20:29 +0200

    skip package build for unsupported archs

--- ci-build.sh
+++ ci-build.sh
@@ -34,11 +34,16 @@ execute 'Add [ci.ri2] respository to pacman.conf' add_ci_ri2_repo
 # Build
 execute 'Approving recipe quality' check_recipe_quality
 for package in "${packages[@]}"; do
-    execute 'Building binary' makepkg-mingw --noconfirm --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild --sign
-#     execute 'Installing' yes:pacman --upgrade *.pkg.tar.zst
-#     execute 'Uninstalling' yes:pacman --remove --recursive --cascade --noconfirm "${package/mingw-w64/mingw-w64-i686}" "${package/mingw-w64/mingw-w64-x86_64}" "${package/mingw-w64/mingw-w64-ucrt-x86_64}"
-    deploy_enabled && mv "${package}"/*.pkg.tar.zst "${package}"/*.pkg.tar.zst.sig artifacts
-#     deploy_enabled && drop_old_bintray_versions "${package}"
+    arch_matches=$( sh -c "source ${package}/PKGBUILD && if [[ \" \${mingw_arch[@]} \" =~ \" ${MINGW_ARCH} \" ]]; then echo yes; fi " )
+    if [[ ${arch_matches} == "yes" ]]; then
+        execute "Building binary for arch ${MINGW_ARCH}" makepkg-mingw --noconfirm --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild --sign
+    #     execute 'Installing' yes:pacman --upgrade *.pkg.tar.zst
+    #     execute 'Uninstalling' yes:pacman --remove --recursive --cascade --noconfirm "${package/mingw-w64/mingw-w64-i686}" "${package/mingw-w64/mingw-w64-x86_64}" "${package/mingw-w64/mingw-w64-ucrt-x86_64}"
+        deploy_enabled && mv "${package}"/*.pkg.tar.zst "${package}"/*.pkg.tar.zst.sig artifacts
+    #     deploy_enabled && drop_old_bintray_versions "${package}"
+    else
+        execute "skip unsupported package arch ${MINGW_ARCH}"
+    fi
     unset package
 done
 
